725ba7182bcb6f743c8a5de97bccee2687dbd7f2
chaoslee <chaos2lee@qq.com>
2026-06-03 16:42:16 +0800

    @produck/kitty-workflow: [ADD] Transaction abstract member symbols in Transaction/Symbol.mjs [ADD] KittyTransaction abstract slots for method/URL/status/finished [REFACTOR] model Transaction accessors via M.Method shape parsers

--- packages/workflow/src/Transaction/Abstract.mjs
+++ packages/workflow/src/Transaction/Abstract.mjs
@@ -1,22 +1,35 @@
-import Abstract from '@produck/es-abstract';
-import { I as _I } from './Symbol.mjs';
+import Abstract, { Member as M } from '@produck/es-abstract';
+import { _I } from './Symbol.mjs';
 
 export default Abstract(
   class KittyTransaction {
-    get URL() {
-      return undefined;
-    }
-
-    get finished() {
-      return undefined;
-    }
-
     get method() {
-      return undefined;
+      return this[_I.METHOD.GET]();
+    }
+
+    get URL() {
+      return this[_I.URL.GET]();
     }
 
     get status() {
-      return undefined;
+      return this[_I.STATUS.GET]();
+    }
+
+    set status(value) {
+      this[_I.STATUS.SET](value);
+    }
+
+    get finished() {
+      return this[_I.FINISHED.GET]();
     }
   },
+  ...[
+    Abstract({
+      [_I.METHOD.GET]: M.Method().returns(M.String),
+      [_I.URL.GET]: M.Method().returns(M.String),
+      [_I.STATUS.GET]: M.Method().returns(M.Number),
+      [_I.STATUS.SET]: M.Method().args(M.Number),
+      [_I.FINISHED.GET]: M.Method().returns(M.Boolean),
+    }),
+  ],
 );
--- packages/workflow/src/Transaction/Symbol.mjs
+++ packages/workflow/src/Transaction/Symbol.mjs
@@ -1,3 +1,29 @@
 import { deepFreeze } from '@produck/deep-freeze-enumerable';
 
 export const I = deepFreeze({});
+
+const _I_METHOD_GET = Symbol('._methodGet');
+const _I_URL_GET = Symbol('._urlGet');
+const _I_STATUS_GET = Symbol('._statusGet');
+const _I_STATUS_SET = Symbol('._statusSet');
+const _I_FINISHED_GET = Symbol('._finishedGet');
+
+export const _I = deepFreeze({
+  METHOD: {
+    GET: _I_METHOD_GET,
+  },
+  URL: {
+    GET: _I_URL_GET,
+  },
+  STATUS: {
+    GET: _I_STATUS_GET,
+    SET: _I_STATUS_SET,
+  },
+  FINISHED: {
+    GET: _I_FINISHED_GET,
+  },
+});
+
+export const S = deepFreeze({});
+
+export const _S = deepFreeze({});
